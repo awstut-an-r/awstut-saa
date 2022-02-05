a208b8ee0c40ef915ffc9941aef1239d62597f0c
awstut-an-r <84276199+awstut-an-r@users.noreply.github.com>
2022-02-05 18:50:28 +0900

    Add files via upload

---
+++ 01/002/service1/main.py
@@ -0,0 +1,39 @@
+import boto3
+import datetime
+import logging
+import os
+import random
+import time
+
+logging.basicConfig(level=logging.INFO)
+
+queue_name = os.environ['QueueName']
+region_name = os.environ['RegionName']
+sqs_endpoint_url = os.environ['SQSEndpointUrl']
+
+sqs = boto3.resource('sqs',
+    region_name=region_name,
+    endpoint_url=sqs_endpoint_url)
+queue = sqs.get_queue_by_name(QueueName=queue_name)
+
+def main():
+    #for i in range(LOOP_COUNT):
+    while True:
+        now = datetime.datetime.now()
+        now_str = now.strftime('%Y%m%d%H%M%S%f')
+        epoch_time = int(time.mktime(now.timetuple()))
+        
+        logging.info(now_str)
+        logging.info(epoch_time)
+        
+        messages = [{
+            'Id': str(now_str),
+            'MessageBody': str(epoch_time)
+        }]
+        response = queue.send_messages(Entries=messages)
+        
+        time.sleep(random.randint(10, 60))
+    
+
+if __name__ == '__main__':
+    main()
---
+++ 01/002/service2/main.py
@@ -0,0 +1,53 @@
+import boto3
+import logging
+import os
+import time
+
+
+logging.basicConfig(level=logging.INFO)
+
+interval = int(os.environ['Interval'])
+queue_name = os.environ['QueueName']
+region_name = os.environ['RegionName']
+sqs_endpoint_url = os.environ['SQSEndpointUrl']
+
+sqs = boto3.resource('sqs',
+    region_name=region_name,
+    endpoint_url=sqs_endpoint_url)
+queue = sqs.get_queue_by_name(QueueName=queue_name)
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+def main():
+    while True:
+        messages = queue.receive_messages(MaxNumberOfMessages=1)
+        # print(messages)
+        # logging.debug(messages)
+        # print('aaaaaaaaaaaaaa')
+        # 1 / 0
+        
+        #if len(messages) == 0:
+        #    # print('no messages')
+        #    logging.info('no messages')
+        #    continue
+        
+        for msg in messages:
+            # print(msg.body)
+            logging.info(msg.body)
+            num = int(msg.body)
+            
+            if is_even(num):
+                logging.info('{num} is even.'.format(num=num))
+            else:
+                logging.info('{num} is odd.'.format(num=num))
+                
+            time.sleep(interval)
+                
+            msg.delete()
+            
+            
+if __name__ == '__main__':
+    main()
